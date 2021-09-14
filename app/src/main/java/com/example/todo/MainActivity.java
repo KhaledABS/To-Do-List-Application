7461e3700355d82ae4b7c8ca1aad7624a7289fce
Khaled <abdessalemkhaled@gmail.com>
2021-09-14 12:58:33 +0200

    Application commit
    This application allows you to add important or non-important tasks to your task list. With a click on one of your tasks, you can mark it as finished or modify it at your convenience !

---
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  static final int SPLASH= 3500;
+    Animation imgAnimation;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        imgAnimation= AnimationUtils.loadAnimation(this,R.anim.todo_img_animation);
+        imageView=findViewById(R.id.todoimg);
+
+        imageView.setAnimation(imgAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH);
+    }
+}
---
+++ app/src/main/java/com/example/todo/RegistrationActivity.java
@@ -0,0 +1,99 @@
+package com.example.todo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private EditText regEmail, regPwd;
+    private Button regBtn;
+    private TextView regLogin;
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog loader;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_registration);
+
+        toolbar= findViewById(R.id.registrationToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Registration");
+
+        mAuth =FirebaseAuth.getInstance();
+        loader = new ProgressDialog(this);
+
+        regEmail=findViewById(R.id.registrationEmail);
+        regPwd= findViewById(R.id.registrationEmail);
+        regBtn= findViewById(R.id.registrationButton);
+        regLogin= findViewById(R.id.registrationLogin);
+
+
+
+
+        regLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = regEmail.getText().toString().trim();
+                String password= regPwd.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    regEmail.setError("email is require");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    regPwd.setError("Password is requiresd");
+                    return;
+                }else {
+                    loader.setMessage(" Register in progress");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Intent intent = new Intent(RegistrationActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+                            }else{
+                                String error= task.getException().toString();
+                                Toast.makeText(RegistrationActivity.this,"Registration failed"+ error,Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+                        }
+                    });
+                }
+                }
+
+
+        });
+    }
+
+}
